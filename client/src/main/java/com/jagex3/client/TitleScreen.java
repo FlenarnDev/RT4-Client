b26db5e97e0f0260ab891960e6e1ea82db275f42
UnknownSeal <Seal@Unknownseal.com>
2026-02-28 09:44:35 +0100

    New TitleScreen.java

---
+++ client/src/main/java/com/jagex3/client/TitleScreen.java
@@ -0,0 +1,14 @@
+package com.jagex3.client;
+
+import deob.JagString;
+import deob.Static28;
+import org.openrs2.deob.annotation.OriginalMember;
+
+public class TitleScreen {
+    @OriginalMember(owner = "com.jagex3.client.client!jm", name = "A", descriptor = "Lclient!na;")
+    private static final JagString aClass100_603 = Static28.parse("");
+    @OriginalMember(owner = "com.jagex3.client.client!jm", name = "z", descriptor = "Lclient!na;")
+    public static JagString loadString = aClass100_603;
+    @OriginalMember(owner = "com.jagex3.client.client!qc", name = "P", descriptor = "I")
+    public static int loadPos = 10;
+}
